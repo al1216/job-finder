71345ab74eb158aca43e876cf147b87932ada151
Aditya Kumar Gupta <adityakumargupta115@gmail.com>
2023-06-14 01:33:15 +0530

    removed unnecessary codes

--- client/src/Pages/Register/index.jsx
+++ client/src/Pages/Register/index.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
-import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 
 export default function Index() {
